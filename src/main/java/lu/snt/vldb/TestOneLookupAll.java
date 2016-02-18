1ba7abd2cc7548c78df15935a7f476bf91aefaf9
Assaad Moawad <assaad.moawad@uni.lu>
2016-02-18 19:25:14 +0100

    update

--- src/main/java/lu/snt/vldb/TestOneLookupAll.java
+++ src/main/java/lu/snt/vldb/TestOneLookupAll.java
@@ -3,10 +3,11 @@ package lu.snt.vldb;
 import org.kevoree.modeling.KCallback;
 import org.kevoree.modeling.KModel;
 import org.kevoree.modeling.KObject;
+import org.kevoree.modeling.extrapolation.impl.DoublePolynomialExtrapolation;
 import org.kevoree.modeling.memory.manager.DataManagerBuilder;
-import org.kevoree.modeling.memory.space.impl.press.PressOffHeapChunkSpace;
 import org.kevoree.modeling.memory.strategy.impl.PressHeapMemoryStrategy;
 import org.kevoree.modeling.memory.strategy.impl.PressOffHeapMemoryStrategy;
+import org.kevoree.modeling.meta.KMetaAttribute;
 import org.kevoree.modeling.meta.KMetaClass;
 import org.kevoree.modeling.meta.KPrimitiveTypes;
 import org.kevoree.modeling.meta.impl.MetaModel;
@@ -14,25 +15,22 @@ import org.kevoree.modeling.scheduler.impl.AsyncScheduler;
 import org.kevoree.modeling.scheduler.impl.DirectScheduler;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.text.DecimalFormat;
 import java.text.NumberFormat;
 import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * Created by assaad on 16/02/16.
  */
-public class TestOne {
+public class TestOneLookupAll {
     public static void main(String[] arg) {
         try {
             final NumberFormat formatter = new DecimalFormat("#0.00");
             final long timeOrigin = 1000;
             MetaModel dynamicMetaModel = new MetaModel("MyMetaModel");
             final KMetaClass sensorMetaClass = dynamicMetaModel.addMetaClass("Sensor");
-            sensorMetaClass.addAttribute("value", KPrimitiveTypes.DOUBLE);
+            final KMetaAttribute attribute= sensorMetaClass.addAttribute("value", KPrimitiveTypes.DOUBLE);
             int threads = Runtime.getRuntime().availableProcessors();
             BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
@@ -64,12 +62,24 @@ public class TestOne {
             }
 
 
+
             // final AtomicLong counter = new AtomicLong(0);
             final CountDownLatch cdt = new CountDownLatch((int) valuesToInsert);
             final CountDownLatch cdt2 = new CountDownLatch((int) valuesToInsert);
             final CountDownLatch cdt3 = new CountDownLatch(1);
             final long[] compare = new long[1];
             compare[0] = 1000000;
+            final int split=Math.min(10,tsp);
+            final int dim=(tsp-1)/split+1;
+
+            final long[][] times = new long[dim][split];
+            int count=0;
+            for(int j=0;j<dim;j++){
+                for(int i=0;i<split;i++){
+                    times[j][i] =timeOrigin+count;
+                    count++;
+                }
+            }
 
             model.connect(new KCallback() {
                 @Override
@@ -83,34 +93,62 @@ public class TestOne {
                     //Insert test
                     long start, end;
                     double speed;
+
                     start = System.nanoTime();
                     final long finalStart2 = start;
-                    for (long i = 0; i < valuesToInsert; i++) {
 
-                        final long ii = i;
-                        final double value = ii * 0.3;
-                        final long uuid = uids[(int) (ii % ts)];
 
-                        model.lookup(0, timeOrigin + ii, uuid, new KCallback<KObject>() {
-                            @Override
-                            public void on(KObject kObject) {
-                                //  System.out.println("Inserting " + ii + " done");
-                                kObject.set(kObject.metaClass().attribute("value"), value);
-                                cdt.countDown();
-                                long x = valuesToInsert - cdt.getCount();
-                                if (x == compare[0]||(x>0&&x%50000000==0)) {
-                                    double end2 = System.nanoTime();
-                                    double speed2 = (end2 - finalStart2);
-                                    double speed3 = speed2 / (x);
-                                    double perm = 1000000.0 / speed3;
-                                    if(x == compare[0]) {
-                                        compare[0] = compare[0] * 2;
+                    for (int i = 0; i < ts; i++) {
+                        final long uuid = uids[i];
+                        for (int j = 0; j < dim; j++) {
+                            final int jj = j;
+                            model.lookupAllTimes(0, times[jj], uuid, new KCallback<KObject[]>() {
+                                @Override
+                                public void on(KObject[] kObjects) {
+                                    double value;
+                                    value = uuid*7 +  0.7*jj;
+                                    for (int k=0;k<kObjects.length;k++) {
+                                        kObjects[k].set(attribute, value);
+                                        if(kObjects[k].now()==1018){
+                                            System.out.println("id is:" + uuid+ " time is: "+times[jj][k]+" value is "+value+" inserted "+(Double) kObjects[k].get(attribute));
+                                            model.lookup(0, 1018, 1, new KCallback<KObject>() {
+                                                @Override
+                                                public void on(KObject kObject) {
+                                                    if(kObject.now()==1018){
+                                                        System.out.println("inside: "+ kObject.get(attribute));
+                                                    }
+
+                                                }
+                                            });
+                                        }
+                                        value += 0.7;
+                                        cdt.countDown();
                                     }
-                                    System.out.println("Count " + (x / 1000000) + "M, insert pace: " + formatter.format(speed3) + " ns/value, avg speed:  " + formatter.format(perm) + " kv/s");
+                                    long x = valuesToInsert - cdt.getCount();
+                                    if (x >= compare[0] || (x > 0 && x % 50000000 == 0)) {
+                                        double end2 = System.nanoTime();
+                                        double speed2 = (end2 - finalStart2);
+                                        double speed3 = speed2 / (x);
+                                        double perm = 1000000.0 / speed3;
+                                        if (x >= compare[0]) {
+                                            compare[0] = compare[0] * 2;
+                                        }
+                                        System.out.println("Count " + (x / 1000000) + "M, insert pace: " + formatter.format(speed3) + " ns/value, avg speed:  " + formatter.format(perm) + " kv/s");
+                                    }
                                 }
-                            }
-                        });
+                            });
+                        }
                     }
+
+                    model.lookup(0, 1018, 1, new KCallback<KObject>() {
+                        @Override
+                        public void on(KObject kObject) {
+                            if(kObject.now()==1018){
+                                System.out.println("outside: "+ kObject.get(attribute));
+                            }
+
+                        }
+                    });
 
 
                     try {
@@ -130,34 +168,45 @@ public class TestOne {
                     start = System.nanoTime();
                     final long finalStart = start;
                     //  counter.set(0);
-                    for (long i = 0; i < valuesToInsert; i++) {
-                        final long ii = i;
-                        final double value = ii * 0.3;
-                        final long uuid = uids[(int) (ii % ts)];
-                        model.lookup(0, timeOrigin + ii, uuid, new KCallback<KObject>() {
-                            @Override
-                            public void on(KObject kObject) {
-                                //   System.out.println("Inserting " + insertCounter[0] + " done");
-                                double v = (Double) kObject.get(kObject.metaClass().attribute("value"));
-                                if (v != value) {
-                                    System.out.println("Error in reading");
-                                }
-                                cdt2.countDown();
-                                long x = valuesToInsert - cdt2.getCount();
-                                if (x == compare[0]||(x>0&&x%50000000==0)) {
-                                    double end2 = System.nanoTime();
-                                    double speed2 = (end2 - finalStart);
-                                    double speed3 = speed2 / (x);
-                                    double perm = 1000000.0 / speed3;
-                                    if(x == compare[0]) {
-                                        compare[0] = compare[0] * 2;
-                                    }
-                                    System.out.println("Count " + (x / 1000000) + "M, read pace: " + formatter.format(speed3) + " ns/value, avg speed:  " + formatter.format(perm) + " kv/s");
 
+
+
+
+
+                    for (int i = 0; i < ts; i++) {
+                        final long uuid = uids[i];
+                        for (int j = 0; j < dim; j++) {
+                            final int jj = j;
+                            model.lookupAllTimes(0, times[jj], uuid, new KCallback<KObject[]>() {
+                                @Override
+                                public void on(KObject[] kObjects) {
+                                    double value;
+                                    value = uuid*7 + 0.7*jj;
+                                    for (int k=0;k<kObjects.length;k++) {
+                                        double v= (Double)kObjects[k].get(attribute);
+
+                                        if(v!=value){
+                                            System.out.println("Error in reading "+ kObjects[k].now()+" id: "+uuid+" expected: "+value+" got: "+v);
+                                        }
+                                        value += 0.7;
+                                        cdt2.countDown();
+                                    }
+                                    long x = valuesToInsert - cdt2.getCount();
+                                    if (x >= compare[0] || (x > 0 && x % 50000000 == 0)) {
+                                        double end2 = System.nanoTime();
+                                        double speed2 = (end2 - finalStart);
+                                        double speed3 = speed2 / (x);
+                                        double perm = 1000000.0 / speed3;
+                                        if (x >= compare[0]) {
+                                            compare[0] = compare[0] * 2;
+                                        }
+                                        System.out.println("Count " + (x / 1000000) + "M, read pace: " + formatter.format(speed3) + " ns/value, avg speed:  " + formatter.format(perm) + " kv/s");
+                                    }
                                 }
-                            }
-                        });
+                            });
+                        }
                     }
+
 
                     try {
                         cdt2.await();
