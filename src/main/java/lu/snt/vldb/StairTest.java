67ae8c7f42b505912c1c40928f6629eafd024891
Assaad Moawad <assaad.moawad@uni.lu>
2016-02-23 12:00:00 +0100

    stair test

--- src/main/java/lu/snt/vldb/StairTest.java
+++ src/main/java/lu/snt/vldb/StairTest.java
@@ -3,6 +3,7 @@ package lu.snt.vldb;
 import org.kevoree.modeling.KCallback;
 import org.kevoree.modeling.KModel;
 import org.kevoree.modeling.KObject;
+import org.kevoree.modeling.KUniverse;
 import org.kevoree.modeling.memory.manager.DataManagerBuilder;
 import org.kevoree.modeling.memory.space.impl.ManualChunkSpaceManager;
 import org.kevoree.modeling.memory.space.impl.press.PressHeapChunkSpace;
@@ -40,8 +41,8 @@ public class StairTest {
 
             System.out.println("Enter the number of point in each step: ");
             input = br.readLine();
-            final int step = Integer.parseInt(input);
-            final int valuesToInsert = stairs * step;
+            final int steps = Integer.parseInt(input);
+            final int valuesToInsert = stairs * steps;
 
             System.out.println("Total to insert is: " + valuesToInsert);
 
@@ -61,7 +62,7 @@ public class StairTest {
             Gaussian g = new Gaussian();
 
             for (int i = 0; i < num; i++) {
-                double[] d = bench(valuesToInsert,step,stairs);
+                double[] d = bench(valuesToInsert,steps,stairs);
                 System.gc();
                 try {
                     Thread.sleep(1000);
@@ -85,22 +86,161 @@ public class StairTest {
             }
             g.print();
 
-
-
-
-
-
         } catch (Exception ex) {
             ex.printStackTrace();
         }
     }
 
-    private static double[] bench(int valuesToInsert, int step, int stairs) {
-        double[] res=new double[2];
+    private static double[] bench(final int valuesToInsert, final int steps, final int stairs) throws InterruptedException {
+        final double[] res = new double[2];
+
+        final CountDownLatch cdt = new CountDownLatch(valuesToInsert);
+        final CountDownLatch cdt2 = new CountDownLatch(valuesToInsert);
+        final CountDownLatch cdt3 = new CountDownLatch(1);
+        final long[] compare = new long[1];
+        compare[0] = 1000000;
 
 
+        final long[][] times = new long[stairs][steps];
+        int count = 0;
+        for (int j = 0; j < stairs; j++) {
+            for (int i = 0; i < steps; i++) {
+                times[j][i] = timeOrigin + count*2;
+                count++;
+            }
+        }
+
+        model.connect(new KCallback() {
+            @Override
+            public void on(Object o) {
+                final KObject object = model.universe(0).time(timeOrigin).create(sensorMetaClass);
+                final long uid = object.uuid();
+                //     System.out.println("Lookup all is now: " + split + " , model connected, objects created, start inserting...");
+
+
+                //Insert test
+                long start, end;
+                double speed;
+
+                start = System.nanoTime();
+                final long finalStart2 = start;
+                long universe=0;
+
+
+                for (int i = 0; i < stairs; i++) {
+                    final int ii=i;
+                    model.lookupAllTimes(universe, times[ii], uid, new KCallback<KObject[]>() {
+                        @Override
+                        public void on(KObject[] kObjects) {
+                            double value;
+                            value = 0.7 * ii;
+                            for (int k = 0; k < kObjects.length; k++) {
+                                kObjects[k].set(attribute, value);
+                                kObjects[k].destroy();
+                                value += 0.5;
+                                cdt.countDown();
+                            }
+                            long x = valuesToInsert - cdt.getCount();
+                            if (x >= compare[0] || (x > 0 && x % 50000000 == 0)) {
+                                double end2 = System.nanoTime();
+                                double speed2 = (end2 - finalStart2);
+                                double speed3 = speed2 / (x);
+                                double perm = 1000000.0 / speed3;
+                                if (x >= compare[0]) {
+                                    compare[0] = compare[0] * 2;
+                                }
+                                System.out.println("Count " + (x / 1000000) + "M, insert pace: " + formatter.format(speed3) + " ns/value, avg speed:  " + formatter.format(perm) + " kv/s");
+                            }
+                        }
+                    });
+                    KUniverse uni = model.universe(i).diverge();
+                    universe=uni.key();
+                    System.out.println("diverging: "+uni.key());
+                }
+
+                try {
+                    cdt.await();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+
+                end = System.nanoTime();
+                speed = (end - start) / (valuesToInsert);
+                double perm = 1000000.0 / speed;
+                res[0] = perm;
+                System.out.println("Count " + (valuesToInsert / 1000000) + "M, insert pace: " + formatter.format(speed) + " ns/value, avg speed:  " + formatter.format(perm) + " kv/s");
+
+                compare[0] = 1000000;
+                start = System.nanoTime();
+                final long finalStart = start;
+                //  counter.set(0);
+
+                for (int i = 0; i < stairs; i++) {
+                    final int ii=i;
+                    model.lookupAllTimes(ii, times[ii], uid, new KCallback<KObject[]>() {
+                        @Override
+                        public void on(KObject[] kObjects) {
+                            double value;
+                            value = 0.7 * ii;
+                            for (int k = 0; k < kObjects.length; k++) {
+                                double v = (Double) kObjects[k].get(attribute);
+                                kObjects[k].destroy();
+                                if (value != v) {
+                                    System.out.println("Error in reading " + kObjects[k].now() + " universe id: " + ii + " expected: " + value + " got: " + v);
+                                }
+                                value += 0.5;
+                                cdt2.countDown();
+                            }
+                            long x = valuesToInsert - cdt2.getCount();
+                            if (x >= compare[0] || (x > 0 && x % 50000000 == 0)) {
+                                double end2 = System.nanoTime();
+                                double speed2 = (end2 - finalStart);
+                                double speed3 = speed2 / (x);
+                                double perm = 1000000.0 / speed3;
+                                if (x >= compare[0]) {
+                                    compare[0] = compare[0] * 2;
+                                }
+                                System.out.println("Count " + (x / 1000000) + "M, read pace: " + formatter.format(speed3) + " ns/value, avg speed:  " + formatter.format(perm) + " kv/s");
+                            }
+                        }
+                    });
+                }
+
+
+                try {
+                    cdt2.await();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                end = System.nanoTime();
+                speed = (end - start) / (valuesToInsert);
+                perm = 1000000.0 / speed;
+                res[1] = perm;
+
+                System.out.println("Count " + (valuesToInsert / 1000000) + "M, read pace: " + formatter.format(speed) + " ns/value, avg speed:  " + formatter.format(perm) + " kv/s");
+                cdt3.countDown();
+
+
+            }
+        });
+
+
+        cdt3.await();
+        final CountDownLatch cdt4 = new CountDownLatch(1);
+        model.disconnect(new KCallback() {
+            @Override
+            public void on(Object o) {
+                System.out.println("Test over");
+                cdt4.countDown();
+            }
+        });
+        cdt4.await();
 
         return res;
+
+
     }
 
 
